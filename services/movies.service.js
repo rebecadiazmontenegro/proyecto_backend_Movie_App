1ec6c97e8b8fe479de83ab103d294de0e0e22426
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-11-14 14:02:39 +0100

    add/Api

--- services/movies.service.js
+++ services/movies.service.js
@@ -1,49 +1,86 @@
 const Movie = require("../models/movies.model");
+const fetchMovie = require("../utils/fetchMovie.utils");
 
 // GET buscar por título
 const getMovieService = async (title) => {
   try {
-    if(!title){
-        return { message: "Por favor, introduce un título"};
-    } else {
-    const movies = await Movie.find({Title: title},"-_id -__v")
-    return movies ;
+    if (!title) {
+      return { message: "Por favor, introduce un título" };
     }
+
+    const movieFromApi = await fetchMovie(title);
+
+    if (movieFromApi) {
+      //const movieApiExist = await Movie.findOne({ Title: movieFromApi.Title });
+
+      return {
+      Title: movieFromApi.Title,
+      Year: movieFromApi.Year,
+      Runtime: movieFromApi.Runtime,
+      Genre: movieFromApi.Genre,
+      Director: movieFromApi.Director,
+      Actors: movieFromApi.Actors,
+      Plot: movieFromApi.Plot,
+      Country: movieFromApi.Country,
+      Poster: movieFromApi.Poster,
+      imdbRating: movieFromApi.imdbRating,
+    };
+    } else {
+    const movies = await Movie.find({ Title: title }, "-_id -__v");
+    return movies;
+  }
   } catch (error) {
     return { message: "Error al obtener las película", details: error.message };
   }
 };
 
-// POST 
+// POST
 
-async function createMovieService({ Title, Year, Runtime, Genre, Director, Actors, Plot, Country, Poster, imdbRating, Opinions }) {
+async function createMovieService({
+  Title,
+  Year,
+  Runtime,
+  Genre,
+  Director,
+  Actors,
+  Plot,
+  Country,
+  Poster,
+  imdbRating,
+  Opinions,
+}) {
   try {
-  const newMovie = new Movie({
-    Title,
-    Year,
-    Runtime,
-    Genre,
-    Director,
-    Actors,
-    Plot,
-    Country,
-    Poster,
-    imdbRating,
-    Opinions
-  });
+    const newMovie = new Movie({
+      Title,
+      Year,
+      Runtime,
+      Genre,
+      Director,
+      Actors,
+      Plot,
+      Country,
+      Poster,
+      imdbRating,
+      Opinions,
+    });
     const savedMovie = await newMovie.save();
     return savedMovie;
-
   } catch (error) {
-    return { error: true, message: "Error al crear el pelicula", details: error.message };
+    return {
+      error: true,
+      message: "Error al crear el pelicula",
+      details: error.message,
+    };
   }
 }
 
 // PUT
 
-const editMovieService = async ({title,datosEditarMovie})=>{
-    return await Movie.findOneAndUpdate(  { Title: title }, datosEditarMovie, { new: true }); //Método de Mongoose que busca un documento por su ID y lo actualiza .findByIdAndUpdate()
-}
+const editMovieService = async ({ title, datosEditarMovie }) => {
+  return await Movie.findOneAndUpdate({ Title: title }, datosEditarMovie, {
+    new: true,
+  }); //Método de Mongoose que busca un documento por su ID y lo actualiza .findByIdAndUpdate()
+};
 
 // DELETE
 
@@ -70,5 +107,5 @@ module.exports = {
   getMovieService,
   createMovieService,
   editMovieService,
-  deleteMovieService
+  deleteMovieService,
 };
